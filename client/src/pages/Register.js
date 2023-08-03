8c6bfda29b20d4e436038be7c48d7d3f87f49289
chiemezie1 <chiemezieagbo1@gmail.com>
2023-08-03 19:55:22 +0100

    client done

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,17 +1,69 @@
-
-import React from 'react';
+import React, { useState } from "react";
 
 function Register() {
+  
+  const [username, setUsername] = useState("");
+  const [fristname, setFristname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function register(ev) {
+    ev.preventDefault();
+    await fetch("http://localhost:4000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        fristname: fristname,
+        lastname: lastname,
+        email: email,
+        password: password,
+      }),
+    });
+  }
+
   return (
     <div>
-      <form className='register' action='' method='post'>
-      <input type='text' name='username' placeholder='Username' />
-        <input type='text' name= 'fristname' placeholder='FristName' />
-        <input type='text' name='lastname' placeholder='LastName' />
-       <input type='email' name='email' placeholder='Email' />
-        <input type='password' name='password' placeholder='Password' />
+      <form className="register" onSubmit={register}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+        />
+        <input
+          type="text"
+          name="fristname"
+          placeholder="FristName"
+          value={fristname}
+          onChange={(ev) => setFristname(ev.target.value)}
+        />
+        <input
+          type="text"
+          name="lastname"
+          placeholder="LastName"
+          value={lastname}
+          onChange={(ev) => setLastname(ev.target.value)}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(ev) => setEmail(ev.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(ev) => setPassword()}
+        />
 
-        <button type='submit'>Register</button>
+        <button type="submit">Register</button>
       </form>
     </div>
   );
