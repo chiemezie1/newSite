0f3d82d063e85b1088ee45e1d18b9d7438cf2322
chiemezie1 <chiemezieagbo1@gmail.com>
2023-08-04 19:27:06 +0100

    MongoDB connected

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,42 @@
 const express = require('express');
-const  mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+
 const app = express();
 
-
 app.use(cors());
+
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
-    
-})
+// MongoDB connection string and options
+const mongoURI = process.env.MONGO_URI;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
+// Connect to MongoDB
+mongoose.connect(mongoURI, options)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 app.post('/register', (req, res) => {
-    const { username, fristname, lastname, email, password } = req.body;
-    res.json({
-        requestData: {
-            username,
-            fristname,
-            lastname,
-            email,
-            password
-        }
-    });
-})
+  const { username, firstname, lastname, email, password } = req.body;
+  // In a production environment, avoid sending the password in the response
+  res.json({
+    requestData: {
+      username,
+      firstname,
+      lastname,
+      email,
+      password,
+    }
+  });
+});
 
-app.listen(4000, () => {
-    console.log('listening on port 4000');
-})
-
-//mongosh "mongodb+srv://cluster10.y7mchtw.mongodb.net/" --apiVersion 1 --username absolute
-//emezie80
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
