08a0e78dd1841b41524214d045b6e79433081598
chiemezie1 <chiemezieagbo1@gmail.com>
2023-08-06 21:18:01 +0100

    update

---
+++ backend/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        min: 4,
+        max: 20
+    },
+    fristname: {
+        type: String,
+        required: true,
+        min: 4,
+        max: 20
+    },
+    lastname: {
+        type: String,
+        required: true,
+        min: 4,
+        max: 20
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 4,
+        max: 20
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 5,
+        max: 50
+    }
+});
+
+const userModel = model('User', UserSchema);
+
+model.exports = userModel;
