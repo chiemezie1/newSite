1f5241f62c13d9dc3d231d81c82b1ca1541721af
chiemezie1 <chiemezieagbo1@gmail.com>
2023-08-07 09:28:05 +0100

    added bcrypt

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const User = require("./models/User.js");
+const bcrypt = require('bcrypt');
 require("dotenv").config();
 
 const app = express();
@@ -23,23 +24,36 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.post("/register", async (req, res) => {
-  const { username, firstname, lastname, email, password } = req.body;
-  try {
-    const userDoc = await User.create({
-      username,
-      firstname,
-      lastname,
-      email,
-      password,
-    });
 
-    res.json(userDoc);
 
-  } catch (err) {
-    res.status(400).json(err)
-   }
-});
+  const saltRounds = 10;
+  
+  app.post("/register", async (req, res) => {
+    const { username, firstname, lastname, email, password } = req.body;
+    try {
+      bcrypt.hash(password, saltRounds, async function (err, hash) {
+        if (err) {
+          // Handle the error if bcrypt.hash encounters an issue
+          console.error('Error while hashing password:', err);
+          return res.status(500).json({ error: 'An error occurred during registration' });
+        }
+  
+        // If no error, proceed with creating the user
+        const userDoc = await User.create({
+          username,
+          firstname,
+          lastname,
+          email,
+          password: hash, 
+        });
+        res.json(userDoc);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+
 
 const PORT = 4000;
 app.listen(PORT, () => {
