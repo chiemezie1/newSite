926a16c71f39e51d4a5e51637f1becdb0cbb407f
chiemezie1 <chiemezieagbo1@gmail.com>
2023-08-14 20:10:20 +0100

    hot fix

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const User = require("./models/User.js");
-const bcrypt = require('bcrypt');
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
 
-app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -29,76 +29,74 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+const saltRounds = 10;
 
+app.post("/register", async (req, res) => {
+  const { username, firstname, lastname, email, password } = req.body;
+  try {
+    bcrypt.hash(password, saltRounds, async function (err, hash) {
+      if (err) {
+        // Handle the error if bcrypt.hash encounters an issue
+        console.error("Error while hashing password:", err);
+        return res
+          .status(500)
+          .json({ error: "An error occurred during registration" });
+      }
 
-  const saltRounds = 10;
-  
-  app.post("/register", async (req, res) => {
-    const { username, firstname, lastname, email, password } = req.body;
-    try {
-      bcrypt.hash(password, saltRounds, async function (err, hash) {
-        if (err) {
-          // Handle the error if bcrypt.hash encounters an issue
-          console.error('Error while hashing password:', err);
-          return res.status(500).json({ error: 'An error occurred during registration' });
-        }
-  
-        // If no error, proceed with creating the user
-        const userDoc = await User.create({
-          username,
-          firstname,
-          lastname,
-          email,
-          password: hash, 
-        });
-        res.json(userDoc);
+      // If no error, proceed with creating the user
+      const userDoc = await User.create({
+        username,
+        firstname,
+        lastname,
+        email,
+        password: hash,
       });
-    } catch (err) {
-      res.status(400).json(err);
+      res.json(userDoc);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDetails = await User.findOne({ username });
+    if (!userDetails) {
+      return res.status(404).json({ error: "User not found" });
     }
-  });
 
+    const hash = userDetails.password;
+    const isPasswordMatch = await bcrypt.compare(password, hash);
 
-  app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-      const userDetails = await User.findOne({ username });
-      if (!userDetails) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-  
-      const hash = userDetails.password;
-      const isPasswordMatch = await bcrypt.compare(password, hash);
-  
-      if (isPasswordMatch) {
-        
-        jwt.sign({ username, id: userDetails._id }, JwtSecret, { }, function(err, token) {
-          if(err) throw err;
-          res.cookie('token', token).json('okay');
+    if (isPasswordMatch) {
+      jwt.sign(
+        { username, id: userDetails._id },
+        JwtSecret,
+        {},
+        function (err, token) {
+          if (err) throw err;
+          res.cookie("token", token).json("okay");
           // console.log(token);
-        });
-
-      } else {
-        // Passwords do not match, authentication failed
-        res.status(401).json({ error: 'Authentication failed' });
-      }
-    } catch (error) {
-      console.error('Error during login:', error);
-      res.status(500).json({ error: 'An error occurred during login' });
+        }
+      );
+    } else {
+      // Passwords do not match, authentication failed
+      res.status(401).json({ error: "Authentication failed" });
     }
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ error: "An error occurred during login" });
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, JwtSecret, {}, (err, infor) => {
+    if (err) throw err;
+    res.json(infor);
   });
-  
-
-  app.get('/profile', (req, res) => {
-    const {token} = req.cookies;
-    jwt.verify(token, JwtSecret, {}, (err, infor) => {
-      if (err) throw err;
-      res.json(infor)
-    }); 
-  })
-
-
-
+});
 
 const PORT = 4000;
 app.listen(PORT, () => {
